8ae63a9aa783a9b74ee495943703ee38c7bf958c
Leonardo Diez <leiodd@gmail.com>
2020-04-16 10:13:58 +0200

    feat: Coverage upload tests, part II

--- .github/upload_coverage.sh
+++ .github/upload_coverage.sh
@@ -4,7 +4,7 @@ DECIDIM_MODULE=$1
 EVENT_PAYLOAD_FILE=$2
 
 PRID=`jq ".number + .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
-SHA=`jq ".pull_request.head.sha + .check_run.pull_requests[0].head.sha + .after" $EVENT_PAYLOAD_FILE`
+SHA=`jq -r ".pull_request.head.sha + .check_run.head_sha + .after" $EVENT_PAYLOAD_FILE`
 
 if [ $PRID = "null" ]
 then
